c6ad067591acc7a2027ec37e380dcfbfcf30d7e8
Xaiier <warrenkcasey@gmail.com>
2015-01-17 19:51:07 -0500

    class setup

---
+++ TimeControl/Flight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace TimeControl
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class Flight : MonoBehaviour
+    {
+        private void Awake()
+        {
+            print("Flight class initialized");
+        }
+    }
+}
---
+++ TimeControl/Warp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace TimeControl
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    class Warp : MonoBehaviour
+    {
+        private void Awake()
+        {
+            print("Warp class initialized");
+        }
+    }
+
+    [KSPAddon(KSPAddon.Startup.TrackingStation, false)]
+    [RequireComponent(typeof(Warp))]
+    class WarpTrackingStation : MonoBehaviour { }
+
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    [RequireComponent(typeof(Warp))]
+    class WarpFlight : MonoBehaviour { }
+}
