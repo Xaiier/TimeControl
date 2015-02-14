49ca03f65a0856873f5f742b61077b27907fcf9b
Xaiier <warrenkcasey@gmail.com>
2015-02-14 03:57:47 -0500

    GUI class

---
+++ TimeControl/GUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace TimeControl
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class GUI : MonoBehaviour
+    {
+        private void Awake()
+        {
+            UnityEngine.Object.DontDestroyOnLoad(this);
+
+            print("GUI class initialized");
+        }
+    }
+}
